42186dfc96825b26424479788a59f95bd12c3a7d
jair-msmz <jairmsmz@outlook.com>
2025-03-28 15:21:54 -0700

    Para Merge2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Sincronizando archivos con S3"
-aws s3 sync src/ s3://jair-bucket-devops-1 --delete --acl public-read
+aws s3 sync src/ s3://jair-bucket-devops-2 --delete --acl public-read
 echo "Despliegue completado"
